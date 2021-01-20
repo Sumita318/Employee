10387cb604260b5c91f0e3cb4ecd3df7ff688b23
Sumita318 <sumitaks318@gmail.com>
2021-01-20 20:47:37 +0545

    ok

---
+++ EmployeeRecord/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeRecord.ViewModel;
+using EmployeeRecord.Models;
+
+namespace EmployeeRecord.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeDBEntities _db = new EmployeeDBEntities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var model = _db.tblEmployeeInfoes.ToList();
+            ViewBag.employee = model;
+            return View();
+        }
+        [HttpPost]
+        public JsonResult SaveEducationDetails(EmployeeViewModel model)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (EmployeeDBEntities db = new EmployeeDBEntities())
+                {
+                    tblEmployeeInfo employeeInfo = new tblEmployeeInfo { EmployeeName = model.EmployeeName, EmployeeAddress = model.EmployeeAddress, ContactNo = model.ContactNo, EmployeeId = model.EmployeeId };
+                    foreach (var i in model.EducationDetails)
+                    {
+                        employeeInfo.tblEducations.Add(i);
+                    }
+                    db.tblEmployeeInfoes.Add(employeeInfo);
+                    db.SaveChanges();
+                    status = true;
+                }
+            }
+            else
+            {
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ EmployeeRecord/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using EmployeeRecord.Models;
+
+namespace EmployeeRecord.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public string ContactNo { get; set; }
+        public List<tblEducation> EducationDetails { get; set; }
+        public virtual tblEducation Education { get; set; }
+    }
+}
